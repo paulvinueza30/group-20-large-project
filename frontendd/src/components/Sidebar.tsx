4708fd502e5600709a546ced10a12d63f000caf2
Maria Fernanda <fefepanda@gmail.com>
2024-10-26 02:45:58 -0400

    frontend set up

---
+++ frontendd/src/components/Sidebar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { GoHome } from "react-icons/go";
+import { PiCardsLight } from "react-icons/pi";
+import { IoPieChartOutline } from "react-icons/io5";
+import { CiUser } from "react-icons/ci";
+import { IoIosLogOut } from "react-icons/io";
+
+function Sidebar() {
+  const layout = [
+    { label: "Dashboard", icon: <GoHome size={30} /> },
+    { label: "Decks", icon: <PiCardsLight size={30} /> },
+    { label: "Stats", icon: <IoPieChartOutline size={30} /> },
+    { label: "Profile", icon: <CiUser size={30} /> },
+    { label: "Logout", icon: <IoIosLogOut size={30} /> },
+  ];
+
+  return (
+    <div className="flex flex-col p-2 bg-slate-200">
+      <div className="pb-6 pt-2">Group 20 Flashcard app</div>
+      {layout.map(({ icon, label }) => (
+        <React.Fragment key={label}>
+          <div className="p-5 flex flex-row">
+            <span className="pr-2">{icon}</span>
+            <button>{label}</button>
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
