ffc3291ab20f3e39394bcba0f0a32d2b95b5c5c5
Ant1304 <ant130404@gmail.com>
2024-10-29 19:10:20 -0400

    Established connection to MongoDB database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
-// TODO : establish MongoDB connections and then run the server
-
 import express from "express";
+import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
 import flashCardRoutes from "./routes/flashCardRoutes"
 import dotenv from "dotenv";
 
+// Load environment variables
 dotenv.config();
 
 const app = express();
@@ -13,10 +13,20 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // User, flashCard routes
-app.use("src/routes/userRoutes.ts", userRoutes);
-app.use("src/routes/flashCardRoutes.ts", flashCardRoutes)
+app.use("/src/routes/userRoutes.ts", userRoutes);
+app.use("/src/routes/flashCardRoutes.ts", flashCardRoutes)
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Get URI from environment variables
+const mongoURI = process.env.MONGODB_URI || "";
+// Connect to MongoDB using URI
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Database connection successful');
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error);
+  });
