a000f33c911e0fe875ebf11ea9efa10f9e17e56c
Paul Vinueza <paulvinueza30@gmail.com>
2024-10-30 14:04:55 -0400

    cleaned up the urls for userApi and added getUserInfo

--- frontend/src/services/userApi.ts
+++ frontend/src/services/userApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const USER_API_URL =
+  `${process.env.REACT_APP_API_URL}/users` || "http://localhost:5000/api/users";
 
 // Register user
 export const registerUser = async (userData: {
@@ -10,7 +11,7 @@ export const registerUser = async (userData: {
   password: string;
 }) => {
   try {
-    const response = await axios.post(`${API_URL}/users/register`, userData, {
+    const response = await axios.post(`${USER_API_URL}/register`, userData, {
       withCredentials: true,
     });
     return response.data;
@@ -27,7 +28,7 @@ export const loginUser = async (userData: {
   password: string;
 }) => {
   try {
-    const response = await axios.post(`${API_URL}/users/login`, userData, {
+    const response = await axios.post(`${USER_API_URL}/login`, userData, {
       withCredentials: true,
     });
     return response.data;
@@ -35,5 +36,16 @@ export const loginUser = async (userData: {
     throw error.response
       ? error.response.data
       : new Error("Internal server error");
+  }
+};
+
+export const getUserInfo = async () => {
+  try {
+    const response = await axios.get(`${USER_API_URL}/user`, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
   }
 };
