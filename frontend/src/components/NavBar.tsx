368f05a00d414148d50bfc79bacc5424b276bccf
Maria Fernanda <fefepanda@gmail.com>
2024-11-05 17:32:22 -0500

    basic layout for home page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -8,12 +8,12 @@ function NavBar() {
             FlashGame
           </span>
         </a>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className="hidden w-full md:block md:w-auto " id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
             <li>
               <a
                 href="/"
-                className="block py-2 px-3 text-blue bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 "
+                className="block py-2 px-3 text-blue bg-blue-700 mr-4 rounded md:bg-transparent md:text-blue-700 md:p-0 "
                 aria-current="page"
               >
                 Home
@@ -22,7 +22,7 @@ function NavBar() {
             <li>
               <a
                 href="/"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 mr-4  md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
               >
                 About
               </a>
@@ -31,7 +31,7 @@ function NavBar() {
             <li>
               <a
                 href="/login"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 bg-secondary"
+                className="block px-6 text-white rounded-md hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 bg-secondary"
               >
                 Log In
               </a>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,26 +1,77 @@
 import NavBar from "../components/NavBar";
+import Placholder from "../assets/Placeholder.png";
+import team from "../assets/Transhumans - Astro.png";
 
 // TODO: Add picture, features, and teams
 function Home() {
   return (
-    <div className="grid grid-cols-6 grid-rows-10 gap-4">
+    <div className="grid grid-cols-6 auto-rows-[100px] gap-4">
       <div className="col-span-4 col-start-2">
         <NavBar />
       </div>
-      <div className="col-span-2 row-span-6 col-start-2 row-start-2">
+      <div className="col-span-2 row-span-5 col-start-2 row-start-2 z-10 pt-10">
         <h1 className="text-8xl font-pixel">Level up your learning</h1>
         <br />
         <h3 className="text-lg pb-10">
           Master any subject with fun, interactive flashcards
         </h3>
-        <a href="/signup" className="rounded bg-primary text-white p-3">
+        <a href="/signup" className="rounded-xl bg-primary text-white p-4 px-6">
           Sign Up Now
         </a>
       </div>
-      <div className="col-span-2 row-span-6 col-start-4 row-start-2 border-2">
-        Image
+      <div className="col-span-3 row-span-5 col-start-3 row-start-2">
+        <img
+          src={Placholder}
+          className="w-max h-max relative left-20 z-0 pt-5"
+        />
       </div>
-      <div className="col-span-6 row-start-8 bg-primary">5</div>
+      {/* Features Part */}
+      <div className="col-span-6 row-span-3 row-start-7 bg-primary h-full col-start-1"></div>
+      <div className="col-span-4 col-start-2 row-span-3 row-start-7 border-2">
+        <h2 className="text-2xl font-bold text-white mt-10">Feature</h2>
+        <div className="flex justify-between pt-10 text-white">
+          <div>
+            <h3 className="text-xl  font-bold">Various learning modes</h3>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Neque donec
+              vitae ipsum facilisi cubilia dictumst.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-xl  font-bold">Streaks and Badged</h3>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Neque donec
+              vitae ipsum facilisi cubilia dictumst.
+            </p>
+          </div>
+          <div className="">
+            <h3 className="text-xl  font-bold">Challenges</h3>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Neque donec
+              vitae ipsum facilisi cubilia dictumst.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-6 row-span-4 row-start-10 border-2">
+        <h2 className="text-center text-4xl text-bold font-pixel py-4 pb-10">
+          Team
+        </h2>
+        <div className="flex justify-evenly">
+          <div className="bg-tertiary border-2 w-[250px] p-0 rounded-2xl overflow-hidden">
+            <img src={team} alt="" className="self-end  relative top-4" />
+            <p className="bg-white">Project Manager</p>
+          </div>
+          <div className="bg-tertiary border-2 w-[250px] p-0 rounded-2xl overflow-hidden">
+            <img src={team} alt="" className="self-end  relative top-4" />
+            <p className="bg-white">Back end</p>
+          </div>
+          <div className="bg-tertiary border-2 w-[250px] p-0 rounded-2xl overflow-hidden">
+            <img src={team} alt="" className="self-end  relative top-4" />
+            <p className="bg-white">Front End</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
