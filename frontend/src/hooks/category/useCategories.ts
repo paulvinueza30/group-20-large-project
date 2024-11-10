c1189b3890c387b4f0b8198b7fc1c72018c70839
Paul Vinueza <paulvinueza30@gmail.com>
2024-11-09 21:40:32 -0500

    fixed useCategory hook to ignoire auth

--- frontend/src/hooks/category/useCategories.ts
+++ frontend/src/hooks/category/useCategories.ts
@@ -13,29 +13,27 @@ interface UseCategoriesResult {
   loading: boolean;
 }
 
-export const useCategories = (
-  isAuthenticated: boolean
-): UseCategoriesResult => {
+export const useCategories = (): UseCategoriesResult => {
   const [data, setData] = useState<Category[] | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchCategories = async () => {
-      if (!isAuthenticated) {
-        setLoading(false);
-        return; // Do not fetch categories if not authenticated
-      }
-
-      setLoading(true); // Start loading
-      setError(null); // Reset any previous errors
+      setLoading(true);
+      setError(null);
 
       try {
-        // Make sure the server is up by pinging the endpoint first (optional)
-        await fetch(`${process.env.REACT_APP_API_URL}/healthcheck`);
+        const healthCheckResponse = await fetch(
+          `${process.env.REACT_APP_API_URL}/healthcheck`
+        );
+        if (!healthCheckResponse.ok) {
+          throw new Error("Server not healthy or user not authenticated");
+        }
 
-        const data = await getAllCategories(); // Fetch categories
-        setData(data); // Set the fetched categories
+        // Fetch categories from the API
+        const fetchedCategories = await getAllCategories();
+        setData(fetchedCategories); // Set the fetched categories
       } catch (error: any) {
         if (axios.isAxiosError(error)) {
           setError(
@@ -45,14 +43,12 @@ export const useCategories = (
           setError("An unexpected error occurred");
         }
       } finally {
-        setLoading(false); // Stop loading after request
+        setLoading(false);
       }
     };
 
-    if (isAuthenticated) {
-      fetchCategories(); // Only fetch categories when authenticated
-    }
-  }, [isAuthenticated]); // Refetch when authentication status changes
+    fetchCategories();
+  }, []);
 
   return { data, error, loading };
 };
