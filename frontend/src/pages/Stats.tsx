359d33c7e16da05943f9abc3552a0ee031a9a7b5
Tmongar98 <tmongar93@gmail.com>
2024-11-24 11:09:07 -0500

    added icons and experience bar for each achievement

--- frontend/src/pages/Stats.tsx
+++ frontend/src/pages/Stats.tsx
@@ -1,19 +1,39 @@
 import SideGrid from "../components/sidebar/SideGrid";
+import {useState, useEffect} from "react"
 import { useUserProfile } from "../context/UserProfileContext";
 import useGetUserAchievements from "../hooks/achievement/useGetAchievement";
+import pdt from "../assets/level1.png"
+import FrameSelection from "../components/FrameSelection";
+
 
 function Stats() {
+  const levelicon = pdt;
+  const { achievements, loading, error } = useGetUserAchievements();
+
+  const [frame, setFrame] = useState<string>(() => {
+    return localStorage.getItem("selectedFrame") || ""; // Get frame from localStorage or default to an empty string
+  });
+
+
+
+  useEffect(() => {
+    if (frame) {
+      localStorage.setItem("selectedFrame", frame); // Persist frame selection to localStorage
+    }
+  }, [frame]);
   const { userProfile } = useUserProfile();
 
   if (!userProfile) {
     return <div>Loading...</div>; // Loading state if profile hasn't been fetched yet
   }
 
-  const { achievements, loading, error } = useGetUserAchievements();
+  
 
   if (loading) {
     return <div>Loading...</div>;
   }
+
+
 
   const Pcolor = userProfile.colorPreferences.primary;
 
@@ -23,11 +43,10 @@ function Stats() {
     Streak: achievements?.filter((ach) => ach.achievementId.type === "Streak"),
   };
   
- 
   return (
    <div className="flex">
         <SideGrid color={Pcolor} />
-      <div className="flex w-full ml-20">
+      <div className="flex w-full ml-20 ">
           <div className="grid grid-cols-6 auto-rows-[100px] gap-4 w-full">
             <div className=" h-full w-full col-span-6">
               <h1 className="font-bold text-2xl dark:text-white pt-14">
@@ -36,25 +55,33 @@ function Stats() {
             </div>
 
             {/*Player Column*/}
-            <div className = "col-span-1 col-start-1 row-span-6">
+            <div className = "flex flex-col col-span-[100px] col-start-1 row-span-6 ">
               {achievements && achievements.length > 0 ? (
                 <ul className = "border-2 m-2 rounded-xl p-3">
-                <h1 className = "font-bold text-2xl">Player</h1>
+                <h1 className = "flex flex-col items-center font-bold text-2xl dark:text-white">Player</h1>
                   {categorizeAchievements.Player?.map((ach) => (
                     <li key = {ach._id} >
-                      <h3 className = "text-xl">
-                        {ach.achievementId.name}
+                      <h3 className = "pt-6 pb-1 flex flex-col items-center text-lg dark:text-white">
+                      {ach.achievementId.name}
                       </h3>
-                      <p className = "text-sm">
-                        {ach.achievementId.description}
-                      </p>
+                      <div className = "flex items-center gap-4">
+                        <div className = "flex-shrink-0">
+                        <img className = "ml-3 items-center flex flex-col w-8 h-8" src = {levelicon}/>
+                        </div>
+                      </div>
+                      <div className="w-36 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 self-center relative bottom-5 ml-12">
+                        <div
+                          className="bg-purple-600 h-2.5 rounded-full dark:bg-purple-500 mb-2"
+                          style={{ width: `${ach.progress}`}}
+                        ></div>
+                      </div>
                     </li>
                   ))}
                 </ul>
               ) : (
                 <p className="text-2xl dark:text-white">
                     No achievements found
-                  </p>
+                </p>
                 )}
                 {error && (
                   <div className="text-xl dark:text-white">Error: {error}</div>
@@ -62,18 +89,26 @@ function Stats() {
             </div>
 
             {/*Deck column*/}
-            <div className = "col-span-1 col-start-2 row-span-6">
+            <div className = "flex flex-col col-span-1 col-start-3 row-span-6">
               {achievements && achievements.length > 0 ? (
                 <ul className = "border-2 m-2 rounded-xl p-3">
-                <h1 className = "font-bold text-2xl">Deck</h1>
+                <h1 className = "flex flex-col items-center font-bold text-2xl dark:text-white">Deck</h1>
                   {categorizeAchievements.Deck?.map((ach) => (
                     <li key = {ach._id} >
-                      <h3 className = "text-xl">
+                      <h3 className = "pt-6 pb-1 flex flex-col items-center text-lg dark:text-white">
                         {ach.achievementId.name}
                       </h3>
-                      <p className = "text-sm">
-                        {ach.achievementId.description}
-                      </p>
+                      <div className = "flex items-center gap-4">
+                        <div className = "flex-shrink-0">
+                        <img className = "ml-3 items-center flex flex-col w-8 h-8" src = {levelicon}/>
+                        </div>
+                      </div>
+                      <div className="w-36 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 self-center relative bottom-5 ml-12">
+                        <div
+                          className="bg-purple-600 h-2.5 rounded-full dark:bg-purple-500 mb-2"
+                          style={{ width: `${ach.progress}`}}
+                        ></div>
+                      </div>
                     </li>
                   ))}
                 </ul>
@@ -88,18 +123,26 @@ function Stats() {
             </div>
 
             {/*Streak column*/}
-            <div className = "col-span-1 col-start-3 row-span-6">
+            <div className = "flex flex-col col-span-1 col-start-5 row-span-6">
               {achievements && achievements.length > 0 ? (
                 <ul className = "border-2 m-2 rounded-xl p-3">
-                <h1 className = "font-bold text-2xl">Streaks</h1>
+                <h1 className = "flex flex-col items-center font-bold text-2xl dark:text-white">Streaks</h1>
                   {categorizeAchievements.Streak?.map((ach) => (
                     <li key = {ach._id} >
-                      <h3 className = "text-xl">
+                      <h3 className = "pt-6 pb-1 flex flex-col items-center text-lg dark:text-white">
                         {ach.achievementId.name}
                       </h3>
-                      <p className = "text-sm">
-                        {ach.achievementId.description}
-                      </p>
+                      <div className = "flex items-center gap-4">
+                        <div className = "flex-shrink-0">
+                        <img className = "ml-3 items-center flex flex-col w-8 h-8" src = {levelicon}/>
+                        </div>
+                      </div>
+                      <div className="w-36 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 self-center relative bottom-5 ml-12">
+                        <div
+                          className="bg-purple-600 h-2.5 rounded-full dark:bg-purple-500 mb-2"
+                          style={{ width: `${ach.progress}`}}
+                        ></div>
+                      </div>
                     </li>
                   ))}
                 </ul>
